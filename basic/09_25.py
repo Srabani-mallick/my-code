9e10655297b06e1e7c8067e09175ee96d0d7b89c
Srabani-Mallick21 <rmallick647@gmail.com>
2026-06-16 08:31:37 +0530

    Add files via upload

---
+++ basic/09_25.py
@@ -0,0 +1,25 @@
+# class Student:
+#     def __init__(self,name,roll):
+#         self.name=name
+#  
+#        self.roll=roll
+import math
+# Square root
+sqrt_value = math.sqrt(16)
+# Power
+pow_value = math.pow(2, 3)
+# Factorial
+factorial_value = math.factorial(5)
+# Ceiling
+ceil_value = math.ceil(4.2)
+# Floor
+floor_value = math.floor(4.8)
+# Absolute value
+fabs_value = math.fabs(-10.5)
+# Output results
+print("Square root of 16:", sqrt_value)
+print("2 raised to the power of 3:", pow_value)
+print("Factorial of 5:", factorial_value)
+print("Ceiling of 4.2:", ceil_value)
+print("Floor of 4.8:", floor_value)
+print("Absolute value of -10.5:", fabs_value)
